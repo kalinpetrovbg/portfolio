b554869d231beff255c96c76e6c9d5513da31570
Kalin Petrov <kalinpetrovbg@gmail.com>
2024-07-27 15:49:48 +0300

    fixing the ignore file

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/image.jpg", width=500)
+
+with col2:
+    st.title("Kalin Petrov")
+    content = """
+    I'm just a regular Python developer.
+    """
+    st.info(content)
+
+main_content = """
+Below you can find some of the apps I have build in Python.
+"""
+st.write(main_content)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.scv", sep=";")
+with col3:
+    for index, row in df.iterrows():
+        if row["id"] % 2 != 0:
+            st.header(row["title"])
+            st.write(row["description"])
+            st.write(row["url"])
+
+with col4:
+    for index, row in df.iterrows():
+        if row["id"] % 2 == 0:
+            st.header(row["title"])
+            st.write(row["description"])
+            st.write(row["url"])
